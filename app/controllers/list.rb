3c2876ffe5bfba0bc38e8e309c823ce7cd794f8f
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-01 11:23:52 -0400

    Add route for creating list

--- app/controllers/list.rb
+++ app/controllers/list.rb
@@ -1,3 +1,12 @@
 get '/user/:user_id/list/all' do
   erb :list_index
 end
+
+get '/user/:user_id/list/:list_id' do
+  @list = List.find(params[:list_id])
+  #add erb later
+end
+
+get '/user/:user_id/list' do
+  erb :create_list
+end
