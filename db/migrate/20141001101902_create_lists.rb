ccf4dbf8b8d93bf5384dc73506f630e3a7d9cb1a
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-01 10:22:54 -0400

    Create tables

--- db/migrate/20141001101902_create_lists.rb
+++ db/migrate/20141001101902_create_lists.rb
@@ -1,4 +1,9 @@
 class CreateLists < ActiveRecord::Migration
   def change
+    create_table :lists do |t|
+      t.string :name
+      t.belongs_to :user
+      t.timestamps
+    end
   end
 end
--- db/migrate/20141001101907_create_items.rb
+++ db/migrate/20141001101907_create_items.rb
@@ -1,4 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
+    create_table :items do |t|
+      t.string :description
+      t.belongs_to :list
+      t.boolean :completed, default: false
+      t.timestamps
+    end
   end
 end
