30622f00f66f3f76cfc2973af4ae908e7c13d7b8
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-01 11:00:13 -0400

    Add error logic to login

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,3 +1,18 @@
 get '/session' do
+  if session[:error]
+    @error = session[:error]
+    session[:error] = nil
+  end
   erb :login
+end
+
+post '/session' do
+  user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+  if user
+    session[:id] = user.id
+    redirect "user/#{user.id}/list/all"
+  else
+    session[:error] = "Login Failed. Please try again."
+    redirect '/session'
+  end
 end
