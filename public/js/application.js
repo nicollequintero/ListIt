1584cdf7559a58aaf1781420ba86b304bf3a1066
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-01 14:48:17 -0400

    Add logic for JS highlighting

--- public/js/application.js
+++ public/js/application.js
@@ -18,15 +18,21 @@ $(document).ready(function() {
 
   $('#add_button').click(function(e) {
     e.preventDefault();
-    // $('body').css('background-color','blue');
     List.addItem($('input[name="description"]').val());
     View.clearItemInput();
   });
 
   $('#save_list_button').click(function(e) {
     e.preventDefault();
-    // $('body').css('background-color','blue');
     List.save();
+  });
+
+  $('#create_items').click(function(e){
+    item = List.items[$(e.target).attr("id") -1];
+    View.highlightItem(item.id);
+    View.populateText(item.description);
+    View.disableAdd();
+    View.disableSave();
   });
 
 });
@@ -38,7 +44,7 @@ var List = {
     this.name = name
   },
   addItem: function(description) {
-    item = new Item(description, this.id)
+    item = new Item(description, this.id, this.items.length + 1)
     this.items.push(item);
     View.printItem(item.id, item.status, item.description);
   },
@@ -49,10 +55,11 @@ var List = {
   }
 }
 
-function Item (description,owner) {
+function Item (description,owner, id) {
   this.description = description;
   this.list = owner;
   this.status = "";
+  this.id = id
 };
 
 Item.prototype.save = function () {
@@ -71,9 +78,23 @@ var View = {
     $('#create_list_form :input').attr('disabled',true);
   },
   printItem: function(id, status, description) {
-    $('#item_list').append('<h5 id="' + id + '">' + description + '</h5>')
+    $('#item_list').append('<div id="' + id + '">' + description + '</div>')
   },
   clearItemInput: function() {
     $('input[name="description"]').val("");
+  },
+  highlightItem: function(id) {
+    $('#item_list').children().removeClass('highlight');
+    $('#'+ id).addClass('highlight');
+  },
+  populateText: function(description) {
+    $('input[name="description"]').val(description);
+  },
+  disableAdd: function() {
+    $('#add_button').prop('disabled',true);
+  },
+  disableSave: function() {
+    $('#save_list_button').prop('disabled',true);
   }
+
 }
