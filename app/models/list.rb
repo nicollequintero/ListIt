195bd9d867ffe28dfdb6320b5b356c9d55d83f95
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-02 00:13:24 -0400

    Add list status logic

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,17 @@
 class List < ActiveRecord::Base
   belongs_to :user
   has_many :items
+
+  def status
+  	status_groups = self.items.group_by { |item| item.completed }
+  	
+  	if (status_groups[false] && status_groups[true])
+  	  return "#{status_groups[true].count} completed out of #{self.items.length}"
+  	elsif (status_groups[false])
+  	  return "All items incomplete"
+  	else
+  	  return "All done"
+  	end
+
+  end
 end
